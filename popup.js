807ea8a3f36cc7560c5833ba4e40532d0bb52b59
Claudiu D <davidescuclaudiu28@gmail.com>
2023-01-22 18:15:11 +0200

    the exported file is sorted and take all history

--- popup.js
+++ popup.js
@@ -11,16 +11,16 @@ let downloadId;
 
 function downloadFile() {
     // Get the browser history
-    chrome.history.search({text: '', maxResults: 1000000, startTime: 0}, function(historyItems) {
+    chrome.history.search({text: '', maxResults: 100000, startTime : 0}, function(historyItems) {
         // Extract the unique domains from the history
         const domains = new Set();
         for (let i = 0; i < historyItems.length; i++) {
             const url = new URL(historyItems[i].url);
             const hostname = url.hostname.split('.');
-            domains.add(hostname[hostname.length - 2] + "." + hostname[hostname.length - 1]);
+            domains.add(hostname.slice(-2).join('.'));
         }
         // Convert the set to an array and join it into a string
-        const domainString = Array.from(domains).join('\n');
+        const domainString = Array.from(domains).sort().join('\n');
         // Create a Blob with the file contents
         const file = new Blob([domainString], {type: 'text/plain'});
         // Create a URL for the file
@@ -33,6 +33,8 @@ function downloadFile() {
         });
     });
 }
+
+
 
 
 
